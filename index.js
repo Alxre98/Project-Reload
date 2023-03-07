0bf67af58d91f0edc4beb0e8607c2b1838ddb6de
Alxre98 <bss.alx08@gmail.com>
2023-03-07 12:30:31 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  window.location.reload();
+})
+
+const h1 = document.querySelector("h1");
+h1.addEventListener("click", () => {
+  h1.style.color = ("green")
+})
